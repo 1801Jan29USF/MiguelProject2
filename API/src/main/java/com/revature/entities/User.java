b333bd9edd8696cdb4180faa7f13a433745974e6
unknown <iperfitt@umich.edu>
2018-03-03 21:27:34 -0800

    database and entities completed

--- API/src/main/java/com/revature/entities/User.java
+++ API/src/main/java/com/revature/entities/User.java
@@ -1,10 +1,15 @@
 package com.revature.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 //@NamedQueries({
@@ -14,27 +19,125 @@ import javax.persistence.SequenceGenerator;
 public class User {
 
 	@Id
-	@Column(name = "user_ID")
-	@SequenceGenerator(name = "userID_SEQ", sequenceName = "userID_SEQ")
-	@GeneratedValue(generator = "userID_SEQ", strategy = GenerationType.AUTO)
-	private int userId;
+	@Column(name = "user_id")
+	@SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq")
+	@GeneratedValue(generator = "user_id_seq", strategy = GenerationType.AUTO)
+	private int user_id;
 
-//	@Column(name = "user_COLOR")
-//	private String userColor;
-//	private String breed;
-//	private double weight;
-//	private double height;
-//
-//	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//	@JoinColumn(name = "cave_id")
-//	private Cave dwelling;
-//
-//	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//	@JoinColumn(name = "HONEYPOT_ID")
-//	private HoneyPot honeyPot;
-//
-//	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//	@JoinTable(name = "PARENT_CUB", joinColumns = @JoinColumn(name = "PARENT_ID"), inverseJoinColumns = @JoinColumn(name = "CUB_ID"))
-//	private Set<user> cubs;
+	@Column(name = "first_name")
+	private String firstname;
+	private String lastname;
+	private String username;
+	private String password;
+	private String address;
+	private String description;
+	private String email;
+	private int phonenumber;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", firstname=" + firstname + ", lastname=" + lastname + ", username="
+				+ username + ", password=" + password + ", address=" + address + ", description=" + description
+				+ ", email=" + email + ", phonenumber=" + phonenumber + ", role=" + role + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + phonenumber;
+		result = prime * result + role;
+		result = prime * result + user_id;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phonenumber != other.phonenumber)
+			return false;
+		if (role != other.role)
+			return false;
+		if (user_id != other.user_id)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_role")
+	private int role;
+
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "event_id")
+	private Event event_id;
+
+	public User(int user_id, String firstname, String lastname, String username, String password, String address,
+			String description, String email, int phonenumber, int role, Event event_id) {
+		super();
+		this.user_id = user_id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.username = username;
+		this.password = password;
+		this.address = address;
+		this.description = description;
+		this.email = email;
+		this.phonenumber = phonenumber;
+		this.role = role;
+		this.event_id = event_id;
+	}
 
 }
