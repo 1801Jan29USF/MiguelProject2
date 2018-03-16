eab456540dea0e4628a2a9562d439b1eb37717a0
unknown <iperfitt@umich.edu>
2018-03-16 16:20:20 -0700

    update

--- Angular 4 template/src/app/app.module.ts
+++ Angular 4 template/src/app/app.module.ts
@@ -24,7 +24,6 @@ import { HostGuard } from './guard/host.guard';
 import { Event } from './beans/event';
 import { SearchpipePipe } from './project2/pipe/searchpipe.pipe';
 import { GenrepipePipe } from './project2/pipe/genrepipe.pipe';
-import { LocationpipePipe } from './project2/pipe/locationpipe.pipe';
 
 
 
@@ -53,8 +52,7 @@ import { LocationpipePipe } from './project2/pipe/locationpipe.pipe';
     Project2loginComponent,
     Project2registerComponent,
     SearchpipePipe,
-    GenrepipePipe,
-    LocationpipePipe,
+    GenrepipePipe
   ],
   providers: [
     CookieService,
